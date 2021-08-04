c530388e1b64a50e9ed957cb79f7bb876235eeb0
Vlad <akame678@gmail.com>
2021-08-05 00:48:37 +0500

    0:48

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,8 @@
-console.log('file 1');
+const price = document.querySelector("#price");
+const priceSpan = document.querySelector("#price-span");
+
+price.addEventListener("input", (e) => {
+	console.log(price.value);
+	priceSpan.textContent = price.value	
+})
+
