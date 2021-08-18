9bdb410ac7163ee278fd6add952e7ba6b53c56d4
Vladislav-Shin <akame678@gmail.com>
2021-08-18 17:37:57 +0500

    calc fix

--- src/assets/js/calculator.js
+++ src/assets/js/calculator.js
@@ -1,26 +1,31 @@
 function calc() {
-	const price = document.querySelector("#quantity");
+	const quantity = document.querySelector("#quantity");
 	const priceSpan = document.querySelector("#amount-span");
 	const selectBtn = document.querySelector(".calculator-select");
-	const calculate = document.querySelector("#calculate");
+	const selectedProduct = document.querySelector("#select-product");
+	document.querySelector(".calculator__price").textContent = selectedProduct.value
+	let priceA = parseInt(quantity.value);
+	let col = parseInt(selectedProduct.value);
 
-	let priceA = 0;
-	let col = 0
-
-	price.addEventListener("input", () => {
-		priceSpan.textContent = price.value;
-		priceA = price.value
+	quantity.addEventListener("input", () => {
+		priceSpan.textContent = quantity.value;
+		priceA = quantity.value;
+		priceFun()
 	})
 
-	selectBtn.addEventListener("change", function () {
+	function priceFun() {
+		const quantity2 = priceA * col;
+		document.querySelector("#total-price").textContent = quantity2
+	}
+
+	function selectFun() {
 		document.querySelector(".calculator__price").textContent = this.value
+		priceA = quantity.value
 		col = this.value
-	})
+		priceFun()
+	}
 
-	calculate.addEventListener("click", () => {
-		const price = priceA * col;
-		document.querySelector("#total-price").textContent = price
-	})
+	selectBtn.addEventListener("change", selectFun)
 }
 
 if (document.querySelector("#quantity")) {
