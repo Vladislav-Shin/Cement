40faa37cb088126ee304c38c46049cd9f93c6c5f
unknown <akame678@gmail.com>
2021-09-29 10:42:33 +0500

    add slider-1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,6 +12,45 @@ var swiper = new Swiper(".mySwiper", {
     },
 
     breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        769: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 40,
+        }
+      },
+  });
+  
+  var swiper = new Swiper(".newsSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
         769: {
           slidesPerView: 3,
           spaceBetween: 20,
